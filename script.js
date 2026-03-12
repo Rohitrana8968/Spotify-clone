bf80e6b249c07b0a695a006548fdeaaa9226104f
Roain <rr5375286@gmail.com>
2026-03-12 10:48:15 +0530

    file added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function details(){
-    let val=await fetch("http://127.0.0.1:8080/");
+    let val=await fetch("http://127.0.0.1:8080/mp3folder/");
     let  res=await val.text();
     console.log(res);
 }
